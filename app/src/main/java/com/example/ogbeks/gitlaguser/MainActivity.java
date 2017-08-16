26be8d2291af3225a9501a7be5fb3d99cd7d5dfb
ogbeks <ogbeks43maan@gmail.com>
2017-08-16 10:06:14 +0100

    Implement the OnItemClickListener for each fake user in the UserAdapter

--- app/src/main/java/com/example/ogbeks/gitlaguser/MainActivity.java
+++ app/src/main/java/com/example/ogbeks/gitlaguser/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.ogbeks.gitlaguser;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -15,10 +19,10 @@ public class MainActivity extends AppCompatActivity {
 
         //create a fake list of github users;
         final ArrayList<User> users = new ArrayList<User>();
-        users.add(new User("Ogbeks", R.drawable.my_pic));
-        users.add(new User("UnicodeDeveloper", R.drawable.dummy_user_pic_web));
-        users.add(new User("sauban", R.drawable.dummy_user_pic_web));
-        users.add(new User("mubanise", R.drawable.dummy_user_pic_web));
+        users.add(new User("Ogbeks", R.drawable.my_pic, "http://www.github.com/ogbeks"));
+        users.add(new User("UnicodeDeveloper", R.drawable.dummy_user_pic_web, "http://www.github.com/unicodedeveloper"));
+        users.add(new User("sauban", R.drawable.dummy_user_pic_web, "http://www.github.com/sauban"));
+        users.add(new User("mubanise", R.drawable.dummy_user_pic_web, "http://www.github.com/mubanise"));
 
         // Create an {@link UserAdapter}, whose data source is a list of
         // {@link User}s. The adapter knows how to create list item views for each item
@@ -28,6 +32,24 @@ public class MainActivity extends AppCompatActivity {
         // Get a reference to the ListView, and attach the adapter to the listView.
         ListView userListView = (ListView) findViewById(R.id.listview_users);
         userListView.setAdapter(usersAdapter);
+
+
+        userListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                User user = users.get(position);
+
+                Intent userProfile = new Intent(MainActivity.this, UserProfileActivity.class);
+
+                Log.v("MainActivity", user.toString());
+                userProfile.putExtra("username", user.getUsername());
+                userProfile.putExtra("userImageResourceId", user.getUserImageUri());
+                userProfile.putExtra("userGitHubUrl", user.getUserGitHubUrl());
+
+                startActivity(userProfile);
+            }
+
+        });
     }
 
 }
